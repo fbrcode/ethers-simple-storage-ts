b72b6780607e07154659570bc116d1a40fcb71f1
fbrcode <fabio.bressler@gmail.com>
2022-06-26 15:07:19 -0300

    setup the deployment script for a specific chain account

--- deploy.js
+++ deploy.js
@@ -1,4 +1,26 @@
-async function main() {}
+const ethers = require("ethers");
+const fs = require("fs-extra");
+
+async function main() {
+  // JSON RPC endpoint = http://0.0.0.0:7545
+  const provider = new ethers.providers.JsonRpcProvider("http://0.0.0.0:7545");
+  const wallet = new ethers.Wallet(
+    "b228573a39720a630c8e6da7f384f3903a699fa386b28345b5d899bbf2ec4ded",
+    provider
+  );
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf-8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf-8"
+  );
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  const contract = await contractFactory.deploy(); // wait for the contract to be deployed
+  // save the contract JSON to file
+  fs.writeFileSync(
+    "./contract-deploy-output.json",
+    JSON.stringify(contract, null, 2)
+  );
+}
 
 main()
   .then(() => process.exit(0))
